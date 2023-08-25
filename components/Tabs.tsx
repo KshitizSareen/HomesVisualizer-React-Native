71de43f8d989dacb81027f5b5ade04797cb651ee
Kshitiz Sareen <Kshitizsareen709@gmail.com>
2023-08-24 22:56:58 -0700

    Update Tabs.tsx

--- components/Tabs.tsx
+++ components/Tabs.tsx
@@ -1,12 +1,13 @@
-import React, {useCallback, useEffect, useState} from 'react';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import React, { useCallback, useEffect, useState } from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faMap } from '@fortawesome/free-regular-svg-icons';
+import { faChartSimple } from '@fortawesome/free-solid-svg-icons';
+
 import Map from './Map';
 import ChartComponent from './ChartComponent';
-import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
-import {faMap} from '@fortawesome/free-regular-svg-icons';
-import {faChartSimple} from '@fortawesome/free-solid-svg-icons';
 
-type pieChartData = {
+type PieChartData = {
   name: string;
   count: number;
   color: string;
@@ -15,60 +16,57 @@ type pieChartData = {
 };
 
 const Tab = createBottomTabNavigator();
+
 const Tabs = () => {
   const [chartsByCategory, setChartsByCategory] = useState([]);
-  const [pieChartData, setPieChartData] = useState([{}]);
+  const [pieChartData, setPieChartData] = useState([]);
   const [lineData, setLineChartData] = useState({});
 
+  const formatCategory = (category) => {
+    return category.split(' ')
+      .filter(value => value)
+      .map(value => value.charAt(0).toUpperCase() + value.slice(1))
+      .join(' ') + 's';
+  }
+
+  const getRandomColor = () => {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
   const formatPieChartData = useCallback(() => {
-    let data: pieChartData[] = [];
-    let labels = [];
-    let avgPriceData = [];
-    let minPriceData = [];
-    let maxPriceData = [];
-    for (let i = 0; i < chartsByCategory.length; i++) {
-      let chartElem: any = chartsByCategory[i];
-      let xValue = chartElem.Category;
-      let splitXValue = xValue.split(' ');
-      let newXValue = [];
-      for (let j = 0; j < splitXValue.length; j++) {
-        let value = splitXValue[j];
-        if (value !== '') {
-          newXValue.push(value[0].toUpperCase() + value.slice(1, value.length));
-        }
-      }
-      let newXValueString = newXValue.join(' ') + 's';
+    const data = [];
+    const labels = [];
+    const avgPriceData = [];
+    const minPriceData = [];
+    const maxPriceData = [];
+
+    for (const chartElem of chartsByCategory) {
+      const formattedCategory = formatCategory(chartElem.Category);
       data.push({
-        name: newXValueString,
+        name: formattedCategory,
         count: chartElem.Count,
         color: getRandomColor(),
         legendFontColor: '#7F7F7F',
         legendFontSize: 12,
       });
-      labels.push(newXValueString);
+      labels.push(formattedCategory);
       avgPriceData.push(chartElem.AvgPrice);
       minPriceData.push(chartElem.MinPrice);
       maxPriceData.push(chartElem.MaxPrice);
     }
+
     setPieChartData(data);
     setLineChartData({
-      labels: labels,
+      labels,
       datasets: [
-        {
-          data: avgPriceData,
-          color: (opacity = 1) => `rgba(255, 0, 0, ${opacity})`, // optional
-          strokeWidth: 2, // optional
-        },
-        {
-          data: minPriceData,
-          color: (opacity = 1) => `rgba(0, 255, 0, ${opacity})`, // optional
-          strokeWidth: 2, // optional
-        },
-        {
-          data: maxPriceData,
-          color: (opacity = 1) => `rgba(0, 0, 255, ${opacity})`, // optional
-          strokeWidth: 2, // optional
-        },
+        { data: avgPriceData, color: (opacity = 1) => `rgba(255, 0, 0, ${opacity})`, strokeWidth: 2 },
+        { data: minPriceData, color: (opacity = 1) => `rgba(0, 255, 0, ${opacity})`, strokeWidth: 2 },
+        { data: maxPriceData, color: (opacity = 1) => `rgba(0, 0, 255, ${opacity})`, strokeWidth: 2 },
       ],
       legend: ['Avg Price', 'Min Price', 'Max Price'],
     });
@@ -78,47 +76,24 @@ const Tabs = () => {
     formatPieChartData();
   }, [chartsByCategory, formatPieChartData]);
 
-  function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
-
   return (
     <Tab.Navigator
-      screenOptions={({route}) => ({
-        tabBarIcon: ({color, size}) => {
-          if (route.name === 'View Listings') {
-            return <FontAwesomeIcon icon={faMap} size={size} color={color} />;
-          }
-          if (route.name === 'Analyze Data') {
-            return (
-              <FontAwesomeIcon icon={faChartSimple} size={size} color={color} />
-            );
-          }
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let icon;
+          if (route.name === 'View Listings') icon = faMap;
+          if (route.name === 'Analyze Data') icon = faChartSimple;
+          return <FontAwesomeIcon icon={icon} size={size} color={color} />;
         },
       })}>
       <Tab.Screen
         name="View Listings"
         component={Map}
-        initialParams={{
-          setChartsByCategory,
-        }}
+        initialParams={{ setChartsByCategory }}
       />
       <Tab.Screen
         name="Analyze Data"
-        children={props => {
-          return (
-            <ChartComponent
-              {...props}
-              pieChartData={pieChartData}
-              lineData={lineData}
-            />
-          );
-        }}
+        children={props => <ChartComponent {...props} pieChartData={pieChartData} lineData={lineData} />}
       />
     </Tab.Navigator>
   );
